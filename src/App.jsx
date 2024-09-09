8ebef4ee20324f42f87964e5f5f874405547dd24
Rohan Thakre <thakrerohan301@gmail.com>
2024-09-09 20:53:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './Componets/Sidebar/Sidebar'
+import Main from './Componets/Main/Main'
+
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+      <Main/>           
+    </>
+  )
+}
+
+export default App
